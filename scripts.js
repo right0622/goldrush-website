65d94646575959db383e3e77b06c5797e5d25412
right0622 <raito795581@gmail.com>
2024-07-04 22:07:10 +0900

    細かい修正

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var thankYouButton = document.getElementById('thankYouButton');
+  var thankYouButton = document.getElementById('thankYouButton'); //要素を取得
   var thankYouMessage = document.getElementById('thankYouMessage');
 
-  thankYouButton.addEventListener('click', function() {
-      thankYouMessage.style.display = 'block';
-      thankYouButton.remove();
+  thankYouButton.addEventListener('click', function() { //クリックされたとき
+      thankYouMessage.style.display = 'block'; //メッセージを表示
+      thankYouButton.remove(); //ボタンを削除
   });
 });
